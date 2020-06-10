9e413c502dc1a13b6bc4454bf5cea3941876ac06
howardwzh <howardwzh@163.com>
2020-06-10 23:00:56 +0800

    init storybook dist

---
+++ precache-manifest.79ee9ae42af0146318577150ee9f18b8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef0cc439c2ee2791a6db6bdf932426a4",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.199190c574a1ef4e2279.bundle.js"
+  },
+  {
+    "url": "runtime~main.199190c574a1ef4e2279.bundle.js"
+  },
+  {
+    "url": "vendors~main.199190c574a1ef4e2279.bundle.js"
+  },
+  {
+    "url": "vendors~main.199190c574a1ef4e2279.bundle.js.LICENSE.txt"
+  }
+]);
