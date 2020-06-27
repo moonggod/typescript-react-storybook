4b5f4ad3d312c86dd200874bb93b02a6b1e3b6c6
futureicoming <moonggod@outlook.com>
2020-06-28 07:55:33 +0800

    add address

---
+++ src/components/Address/_types.ts
@@ -0,0 +1,60 @@
+import {
+    IsOptional,
+    IsNotEmpty,
+    IsString,
+    IsPhoneNumber,
+    IsInt,
+    IsBoolean
+  } from 'class-validator'
+  
+  export class AddressWithoutId {
+    @IsNotEmpty()
+    @IsString()
+    firstname: string = ''
+  
+    @IsNotEmpty()
+    @IsString()
+    lastname: string = ''
+  
+    @IsOptional()
+    @IsString()
+    company: string = ''
+  
+    @IsNotEmpty()
+    @IsPhoneNumber('US') // TODO: US only? Should allow other countries?
+    telephone: string = '' // TODO: It should be 2 - 3 parts
+  
+    @IsNotEmpty()
+    @IsString()
+    street: string = ''
+  
+    @IsNotEmpty()
+    @IsString()
+    city: string = '' // TODO: it should be an ID or code instead of any user input
+  
+    @IsInt()
+    region_id: number = 0
+  
+    @IsOptional()
+    @IsString()
+    postcode: string = '' // TODO: is it necessary?
+  
+    @IsString()
+    country_id: string = '' // TODO: range (min, max)? enum?
+  
+    @IsBoolean()
+    default_billing?: boolean = false
+  
+    @IsBoolean()
+    default_shipping?: boolean = false
+  }
+  
+  export class Address extends AddressWithoutId {
+    @IsInt()
+    id!: number
+  }
+  
+  export class AddressdRes {
+    @IsBoolean()
+    data: boolean = true
+  }
